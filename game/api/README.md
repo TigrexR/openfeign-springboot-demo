2d2ef80140ea2bfd2e8a6935c7263eed0c9804f2
TigrexR <swcdasd@sina.com>
2021-11-05 22:58:04 +0800

    create all

---
+++ game/api/README.md
@@ -0,0 +1,9 @@
+## Open Feign RPC框架
+- jdk11
+- hystrix、ribbon、feign
+- hikariCP、mybatis、sharding-jdbc
+- redis、elasticsearch、mongodb
+- jackson
+- groove
+- kafka
+- nacos
---
+++ news/service/src/main/java/com/tigrex/news/service/NewsApplication.java
@@ -0,0 +1,13 @@
+package com.tigrex.news.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApplication.class, args);
+	}
+
+}
