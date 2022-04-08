c13cf177e67eb724a4d64bc615a376ac5faa30aa
TigrexR <swcdasd@sina.com>
2022-04-08 23:47:06 +0800

    open feign

--- team/service/src/main/java/com/tigrex/team/service/mapper/TeamMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tigrex.team.service.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tigrex.team.service.po.TeamDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TeamMapper extends BaseMapper<TeamDO> {
-}
--- user/api/README.md
+++
@@ -1,9 +0,0 @@
-## Open Feign RPC框架
-- jdk11
-- hystrix、ribbon、feign
-- hikariCP、mybatis、sharding-jdbc
-- redis、elasticsearch、mongodb
-- jackson
-- groove
-- kafka
-- nacos
